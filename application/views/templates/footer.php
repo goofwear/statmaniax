18f40f51b22d20228243fcc20108400d7bc7dcc7
Orkut <mrorkut@gmx.co.uk>
2018-08-30 15:35:43 +0100

    some copyright stuff

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,4 @@
-<p class="copyright">StepmaniaX is &copy;2018 Step Revolution LLC.</p>
+<p class="copyright">StepmaniaX is &copy;2018 Step Revolution LLC. StatManiaX is developed by <a
+            href="mailto:liam@namwen.me">Namwen</a> and <a href="http://concubidated.com">Concubidated</a> .</p>
 </body>
 </html> 
