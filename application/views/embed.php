689c3ecb61d2ae6c5850c2b3eb3e4bbb490fee0a
Namwen <liam@namwen.me>
2018-09-05 18:55:05 +0100

    favicon

--- application/views/embed.php
+++ application/views/embed.php
@@ -4,6 +4,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
+    <link rel="icon" type="image/png" href="<?= base_url('assets/img/favicon.png') ?>"/>
 
     <link rel="stylesheet" href="/assets/css/footable.bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Quicksand:300" rel="stylesheet">
